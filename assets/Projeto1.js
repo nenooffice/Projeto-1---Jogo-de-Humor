9a3c9d973e90364a0e6163665ff71dba94478db9
Neno <eugenio.bufalorodrigues@gmail.com>
2022-04-19 20:17:17 -0300

    Update Projeto1.js

--- assets/Projeto1.js
+++ assets/Projeto1.js
@@ -2,8 +2,6 @@ const botao_m = document.querySelector("#botao_m");
 const humor_m = document.querySelector("#humor_m");
 let imagem_m = document.getElementById("imagem_m");
 
-let a = 0;
-
 
 botao_m.addEventListener("mouseover", function(){
     botao_m.style.backgroundColor = "grey";
@@ -15,25 +13,24 @@ botao_m.addEventListener("mouseout", function(){
 
 botao_m.addEventListener("click", function(){
   
-  if(a == 0){
+  if(imagem_m.src.match("./assets/preto.png")){
   humor_m.innerText = "Vermelho";
   imagem_m.src = "./assets/vermelho.png";
 }
-  else if (a == 1){
+  else if (imagem_m.src.match("./assets/vermelho.png")){
   humor_m.innerText = "Amarelo";
   imagem_m.src = "./assets/amarelo.png";
 }
-  else if (a == 2){
+  else if (imagem_m.src.match("./assets/amarelo.png")){
   humor_m.innerText = "Verde";
   imagem_m.src = "./assets/verde.png";
 }
-  else if (a == 3){
+  else if (imagem_m.src.match("./assets/verde.png")){
   humor_m.innerText = "Azul";
   imagem_m.src = "./assets/azul.png";
 }
-  a++;
-
-  if(a == 4){
-    a = 0;
-  }
+  else if (imagem_m.src.match("./assets/azul.png")){
+  humor_m.innerText = "Cor";
+  imagem_m.src = "./assets/preto.png";
+}
 });
